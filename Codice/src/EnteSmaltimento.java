ce6b8cad1b688bdf7e45fd55f46e62b901cc38d1
Andrea Falaschini <44972556+fala-flash@users.noreply.github.com>
2018-11-13 17:27:39 +0100

    Update Codice/src/EnteSmaltimento.java

---
+++ Codice/src/EnteSmaltimento.java
@@ -0,0 +1,10 @@
+public class EnteSmaltimento {
+    private String nome;
+
+    public EnteSmaltimento(){
+      //  Tecnico Luigi = new Tecnico();
+       // OperatoreEcologico Mario = new OperatoreEcologico();
+    }
+
+
+}
