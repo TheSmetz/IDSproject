edfea30c8304e7f52f5cf2e96f3686661bd34054
Andrea Falaschini <44972556+fala-flash@users.noreply.github.com>
2018-11-13 17:27:49 +0100

    Update Codice/src/Tecnico.java

---
+++ Codice/src/Tecnico.java
@@ -0,0 +1,10 @@
+public class Tecnico extends EnteSmaltimento {
+    private String nome;
+    private String ID;
+
+    public Tecnico(){}; //costruttore
+
+    public void installaCestino(){}
+    public void risoluzioneProblemi(){}
+
+}
