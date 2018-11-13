b2e0d65060100957c149d9c4a62657e143df1303
Andrea Falaschini <44972556+fala-flash@users.noreply.github.com>
2018-11-13 17:27:32 +0100

    Update Codice/src/Cestino.java

---
+++ Codice/src/Cestino.java
@@ -0,0 +1,11 @@
+public class Cestino extends CestinoSmart{
+    private int capienza; //mostra percentuale di riempimento del cestino
+    private boolean sensore = false; //controlla se è stato gettato un rifiuto
+
+    public Cestino(){} //costruttore
+
+    public void getCapienza(){}
+    public void setCapienza(){}
+    public void getSensore(){}
+    public void setSensore(){}
+}
