8dff8339fc2cb507284c86a97596d283e123890f
Andrea Falaschini <44972556+fala-flash@users.noreply.github.com>
2018-11-13 17:27:58 +0100

    Update Codice/src/Timer.java

---
+++ Codice/src/Timer.java
@@ -0,0 +1,14 @@
+public class Timer {
+   private final int timerBocchetta = 10;
+   private final int timerOperazione = 20;
+   private boolean sessione = false;
+
+   public Timer(){} //costruttore
+
+   private void startTimerBocchetta(){}
+   private void startTimerOperazione(){}
+   private void startSessione(){}
+   private void ripristinoSessione(){}
+   public void notifyTimerSessione(){}
+
+}
