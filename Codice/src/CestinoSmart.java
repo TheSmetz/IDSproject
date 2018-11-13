732bd452a1a104fceda3f56af82fc450acc0879e
Andrea Falaschini <44972556+fala-flash@users.noreply.github.com>
2018-11-13 17:27:36 +0100

    Update Codice/src/CestinoSmart.java

---
+++ Codice/src/CestinoSmart.java
@@ -0,0 +1,11 @@
+public class CestinoSmart {
+    private boolean integrita = false; //false se il sistema è malfunzionante
+    private boolean bocchetta = false;//false se la bocchetta è chiusa
+
+    public CestinoSmart(){} //costruttore
+
+    public void getIntegrita(){}
+    public void setIntegrita(){}
+    public void chiamaAssistenza(){}
+    public void scansionaTessera(){}
+}
