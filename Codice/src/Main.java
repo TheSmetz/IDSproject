e43c0e18b8a00c8717adcc6ca0b064b0931f320e
Andrea Falaschini <44972556+fala-flash@users.noreply.github.com>
2018-11-13 18:08:52 +0100

    Update Codice/src/Main.java

--- Codice/src/Main.java
+++ Codice/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main (String[] args){
-        System.out.print("Git Test");
+        Prodotto Birra = new Prodotto("g313v12g3", "Heineken",
+                "Tappo indifferenziato, bottiglia vetro", false,
+                false, true, true);
+        Prodotto Giornale = new Prodotto("hwgyue23", "Gazzetta",
+                "Giornale carta", true, false, false,
+                false);
+
+        Birra.getMateriale();
+        Birra.getCodiceABarre();
+        Birra.getDescrizione();
+        Giornale.getMateriale();
+        Giornale.getCodiceABarre();
+        Giornale.getDescrizione();
+
     }
 }
