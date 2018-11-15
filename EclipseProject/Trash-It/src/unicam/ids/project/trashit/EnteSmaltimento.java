ec3d27fa93558d3eff4472b1fa84d1ff2890832f
Andrea Falaschini <44972556+fala-flash@users.noreply.github.com>
2018-11-15 18:28:10 +0100

    Update EclipseProject/Trash-It/src/unicam/ids/project/trashit/EnteSmaltimento.java

---
+++ EclipseProject/Trash-It/src/unicam/ids/project/trashit/EnteSmaltimento.java
@@ -0,0 +1,18 @@
+package unicam.ids.project.trashit;
+
+public class EnteSmaltimento {
+	private String nome;
+
+	public EnteSmaltimento() {
+		// Tecnico Luigi = new Tecnico();
+		// OperatoreEcologico Mario = new OperatoreEcologico();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
