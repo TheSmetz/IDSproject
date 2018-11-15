a6e45fd89c665ab9c96df9934a387e5754c5c25f
Andrea Falaschini <44972556+fala-flash@users.noreply.github.com>
2018-11-15 18:28:30 +0100

    Update EclipseProject/Trash-It/src/unicam/ids/project/trashit/Timer.java

---
+++ EclipseProject/Trash-It/src/unicam/ids/project/trashit/Timer.java
@@ -0,0 +1,41 @@
+package unicam.ids.project.trashit;
+
+public class Timer {
+	private final int timerBocchetta = 10;
+	private final int timerOperazione = 20;
+	private boolean sessione = false;
+
+	public Timer() {
+	} // costruttore
+
+	private void startTimerBocchetta() {
+	}
+
+	private void startTimerOperazione() {
+	}
+
+	private void startSessione() {
+	}
+
+	private void ripristinoSessione() {
+	}
+
+	public void notifyTimerSessione() {
+	}
+
+	public boolean isSessione() {
+		return sessione;
+	}
+
+	public int getTimerBocchetta() {
+		return timerBocchetta;
+	}
+
+	public int getTimerOperazione() {
+		return timerOperazione;
+	}
+
+	public void setSessione(boolean sessione) {
+		this.sessione = sessione;
+	}
+}
