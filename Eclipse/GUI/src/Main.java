b0ddb498567e77f94bc6b87c41c4854b9a3e4e64
fala-flash <44972556+fala-flash@users.noreply.github.com>
2018-11-25 11:09:44 +0100

    Update Main.java

--- Eclipse/GUI/src/Main.java
+++ Eclipse/GUI/src/Main.java
@@ -28,6 +28,7 @@ public class Main extends JFrame {
 				try {
 					Main frame = new Main();
 					frame.setVisible(true);
+					frame.setResizable(false);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
