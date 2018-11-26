c1df6038268216cf3694a83201f144637baed0bd
d-mich <40661164+d-mich@users.noreply.github.com>
2018-11-26 15:17:12 +0100

    Create OperatoreEcologico.java

---
+++ Eclipse/Trash-it/src/Console/OperatoreEcologico.java
@@ -0,0 +1,28 @@
+package Console;
+
+public class OperatoreEcologico extends EnteSmaltimento {
+	private String nome;
+	private String ID;
+
+	public OperatoreEcologico() {
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void ritiroCestinoPieno() {
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+}
