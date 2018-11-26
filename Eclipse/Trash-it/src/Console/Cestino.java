3c4a25dfd488eb74d3c6899d8c6c70f1b2cc8c31
d-mich <40661164+d-mich@users.noreply.github.com>
2018-11-26 15:17:00 +0100

    Create Cestino.java

---
+++ Eclipse/Trash-it/src/Console/Cestino.java
@@ -0,0 +1,25 @@
+package Console;
+
+public class Cestino extends CestinoSmart {
+	private int capienza; // mostra percentuale di riempimento del cestino
+	private boolean sensore = false; // controlla se è stato gettato un rifiuto
+
+	public Cestino() {
+	} // costruttore
+
+	public int getCapienza() {
+		return capienza;
+	}
+
+	public void setCapienza(int capienza) {
+		this.capienza = capienza;
+	}
+
+	public boolean isSensore() {
+		return sensore;
+	}
+
+	public void setSensore(boolean sensore) {
+		this.sensore = sensore;
+	}
+}
