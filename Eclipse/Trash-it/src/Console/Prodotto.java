13972148fd8a1b7dcd9f5fc88563f3689e460684
d-mich <40661164+d-mich@users.noreply.github.com>
2018-11-26 15:17:14 +0100

    Create Prodotto.java

---
+++ Eclipse/Trash-it/src/Console/Prodotto.java
@@ -0,0 +1,59 @@
+package Console;
+
+import java.util.ArrayList;
+
+public class Prodotto {
+	private String codiceABarre;
+	private String nomeProdotto;
+	private String descrizione;
+	private boolean materialeCarta = false;
+	private boolean materialePlastica = false;
+	private boolean materialeVetro = false;
+	private boolean materialeIndifferenziato = false;
+	private ArrayList<String> materiali = new ArrayList<>(); // lista di materiali del prodotto inserito
+
+	public Prodotto(String codiceABarre, String nomeProdotto, String descrizione, boolean materialeCarta,
+			boolean materialePlastica, boolean materialeVetro, boolean materialeIndifferenziato) {
+		this.codiceABarre = codiceABarre;
+		this.nomeProdotto = nomeProdotto;
+		this.descrizione = descrizione;
+		this.materialeCarta = materialeCarta;
+		this.materialePlastica = materialePlastica;
+		this.materialeVetro = materialeVetro;
+		this.materialeIndifferenziato = materialeIndifferenziato;
+	} // costruttore
+
+	public void getCodiceABarre() {
+		// return this.codiceABarre;
+		System.out.println(this.codiceABarre);
+	}
+	
+	public void getNomeProdotto() {
+		// return nomeProdotto
+		System.out.println(this.nomeProdotto);
+	}
+
+	public void getDescrizione() {
+		// return descrizione;
+		System.out.println(this.descrizione);
+	}
+
+	public void getMateriale() { // da controllare efficienza
+		if (this.materialeCarta) {
+			materiali.add("Carta");
+		}
+		if (this.materialeIndifferenziato) {
+			materiali.add("Indifferenziato");
+		}
+		if (this.materialePlastica) {
+			materiali.add("Plastica");
+		}
+		if (this.materialeVetro) {
+			materiali.add("Vetro");
+		}
+		System.out.println(materiali);
+		;
+	} // specifica i/il tipo/i di materiale/i del prodotto
+
+	
+}
