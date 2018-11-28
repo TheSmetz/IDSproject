e3844262a9bee7fc9d98d2da5d69d67d82e0f894
d-mich <40661164+d-mich@users.noreply.github.com>
2018-11-28 15:05:00 +0100

    a

--- Eclipse/Trash-it/src/Gui/GuiMain.java
+++ Eclipse/Trash-it/src/Gui/GuiMain.java
@@ -21,6 +21,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.Insets;
 
 import javax.swing.ImageIcon;
 import java.awt.Color;
@@ -28,7 +29,7 @@ import javax.swing.SwingConstants;
 import java.awt.Toolkit;
 
 @SuppressWarnings("serial")
-public class GuiMain extends JFrame {
+public class Main extends JFrame {
 
 	private JPanel contentPane;
 	private JLayeredPane layeredPane;
@@ -40,7 +41,7 @@ public class GuiMain extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					GuiMain frame = new GuiMain();
+					Main frame = new Main();
 					frame.setLocationRelativeTo(null);
 			        frame.setResizable(false);
 					frame.setVisible(true);
@@ -65,18 +66,18 @@ public class GuiMain extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public GuiMain() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage(GuiMain.class.getResource("/Gui/images/icon.png")));
+	public Main() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Main.class.getResource("/Gui/images/aaa.png")));
 		setTitle("Trash-it");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1315, 794);
+		setBounds(100, 100, 1062, 794);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		layeredPane = new JLayeredPane();
-		layeredPane.setBounds(59, 51, 1183, 658);
+		layeredPane.setBounds(0, 51, 1045, 683);
 		contentPane.add(layeredPane);
 		layeredPane.setLayout(new CardLayout(0, 0));
 		
@@ -87,22 +88,17 @@ public class GuiMain extends JFrame {
 		
 		JLabel lblBenvenuti = new JLabel("BENVENUTO");
 		lblBenvenuti.setHorizontalAlignment(SwingConstants.CENTER);
-		lblBenvenuti.setForeground(Color.WHITE);
+		lblBenvenuti.setForeground(Color.BLACK);
 		lblBenvenuti.setFont(new Font("Segoe UI Semibold", Font.BOLD, 30));
-		lblBenvenuti.setBounds(0, 50, 710, 57);
+		lblBenvenuti.setBounds(416, 0, 629, 57);
 		homePanel.add(lblBenvenuti);
 		
 		JLabel lblInfo = new JLabel("info");
-		lblInfo.setForeground(Color.WHITE);
+		lblInfo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblInfo.setForeground(Color.BLACK);
 		lblInfo.setFont(new Font("Segoe UI Semibold", Font.BOLD, 30));
-		lblInfo.setBounds(926, 565, 73, 57);
+		lblInfo.setBounds(0, 571, 418, 57);
 		homePanel.add(lblInfo);
-		
-		JLabel lblTrashit = new JLabel("TRASH-IT");
-		lblTrashit.setForeground(Color.WHITE);
-		lblTrashit.setFont(new Font("Segoe UI Semibold", Font.BOLD, 30));
-		lblTrashit.setBounds(874, 50, 183, 57);
-		homePanel.add(lblTrashit);
 		
 		
 		JPanel scanPanel = new JPanel();
@@ -115,17 +111,18 @@ public class GuiMain extends JFrame {
 		gttPanel.setOpaque(false);
 		gttPanel.setLayout(null);
 		
-		JLabel lblGettareProdotto = new JLabel("GETTARE PRODOTTO");
-		lblGettareProdotto.setForeground(Color.WHITE);
+		JLabel lblGettareProdotto = new JLabel("CONFERIMENTO PRODOTTO");
+		lblGettareProdotto.setForeground(Color.BLACK);
 		lblGettareProdotto.setHorizontalAlignment(SwingConstants.CENTER);
 		lblGettareProdotto.setFont(new Font("Segoe UI Semibold", Font.BOLD, 30));
-		lblGettareProdotto.setBounds(0, 50, 710, 57);
+		lblGettareProdotto.setBounds(416, 0, 629, 57);
 		gttPanel.add(lblGettareProdotto);
 		
 		JLabel lblGettareProdottoNella = new JLabel("Gettare Prodotto nella bocchetta");
-		lblGettareProdottoNella.setForeground(Color.WHITE);
+		lblGettareProdottoNella.setHorizontalAlignment(SwingConstants.CENTER);
+		lblGettareProdottoNella.setForeground(Color.BLACK);
 		lblGettareProdottoNella.setFont(new Font("Segoe UI Semibold", Font.BOLD, 25));
-		lblGettareProdottoNella.setBounds(155, 221, 399, 57);
+		lblGettareProdottoNella.setBounds(417, 221, 628, 57);
 		gttPanel.add(lblGettareProdottoNella);
 		
 		JButton btnProblemiAssistenza = new JButton("Problemi? Assistenza");
@@ -141,119 +138,124 @@ public class GuiMain extends JFrame {
 		btnProblemiAssistenza.setBounds(211, 482, 244, 57);
 		gttPanel.add(btnProblemiAssistenza);
 		
-		JLabel label_5 = new JLabel("TRASH-IT");
-		label_5.setForeground(Color.WHITE);
-		label_5.setFont(new Font("Segoe UI Semibold", Font.BOLD, 30));
-		label_5.setBounds(874, 50, 183, 57);
-		gttPanel.add(label_5);
-		
 		JLabel label_6 = new JLabel("info");
-		label_6.setForeground(Color.WHITE);
+		label_6.setHorizontalAlignment(SwingConstants.CENTER);
+		label_6.setForeground(Color.BLACK);
 		label_6.setFont(new Font("Segoe UI Semibold", Font.BOLD, 30));
-		label_6.setBounds(926, 565, 73, 57);
+		label_6.setBounds(0, 571, 418, 57);
 		gttPanel.add(label_6);
 		
 		JLabel label_4 = new JLabel("");
-		label_4.setIcon(new ImageIcon(GuiMain.class.getResource("/Gui/images/logo.png")));
-		label_4.setBounds(760, 117, 397, 414);
+		label_4.setHorizontalAlignment(SwingConstants.CENTER);
+		label_4.setIcon(new ImageIcon(Main.class.getResource("/Gui/images/logo.png")));
+		label_4.setBounds(0, 0, 418, 488);
 		gttPanel.add(label_4);
 		
 		JLabel lblScansioneProdotto = new JLabel("SCANSIONE PRODOTTO");
 		lblScansioneProdotto.setHorizontalAlignment(SwingConstants.CENTER);
-		lblScansioneProdotto.setForeground(Color.WHITE);
+		lblScansioneProdotto.setForeground(Color.BLACK);
 		lblScansioneProdotto.setFont(new Font("Segoe UI Semibold", Font.BOLD, 30));
-		lblScansioneProdotto.setBounds(0, 50, 710, 57);
+		lblScansioneProdotto.setBounds(416, 0, 629, 57);
 		scanPanel.add(lblScansioneProdotto);
 		
-		JButton btnProdottoVisualizzatoCorretto = new JButton("SI");
+		
+		//prodotto corretto
+		
+		ImageIcon corretto = new ImageIcon(Main.class.getResource("/Gui/images/whitebutton.png"));
+		JButton btnProdottoVisualizzatoCorretto = new JButton("Prodotto errato", corretto);
+		btnProdottoVisualizzatoCorretto.setVerticalTextPosition(JButton.CENTER);
+		btnProdottoVisualizzatoCorretto.setHorizontalTextPosition(JButton.CENTER);		
+		btnProdottoVisualizzatoCorretto.setBorderPainted(false);
+		btnProdottoVisualizzatoCorretto.setMargin(new Insets(0, 0, 0, 0));
+		btnProdottoVisualizzatoCorretto.setFont(new Font("Segoe UI Semibold", Font.BOLD, 20));
+		btnProdottoVisualizzatoCorretto.setForeground(Color.BLACK);
+		btnProdottoVisualizzatoCorretto.setContentAreaFilled(false);
 		btnProdottoVisualizzatoCorretto.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				switchPanel(gttPanel);
 			}
 		});
 		btnProdottoVisualizzatoCorretto.setOpaque(false);
-		btnProdottoVisualizzatoCorretto.setForeground(Color.WHITE);
+		btnProdottoVisualizzatoCorretto.setForeground(Color.BLACK);
 		btnProdottoVisualizzatoCorretto.setFont(new Font("Segoe UI Semibold", Font.BOLD, 20));
 		btnProdottoVisualizzatoCorretto.setContentAreaFilled(false);
 		btnProdottoVisualizzatoCorretto.setBorderPainted(false);
-		btnProdottoVisualizzatoCorretto.setBounds(220, 543, 79, 57);
+		btnProdottoVisualizzatoCorretto.setBounds(417, 549, 628, 57);
 		scanPanel.add(btnProdottoVisualizzatoCorretto);
 		
-		JLabel label_1 = new JLabel("TRASH-IT");
-		label_1.setForeground(Color.WHITE);
-		label_1.setFont(new Font("Segoe UI Semibold", Font.BOLD, 30));
-		label_1.setBounds(874, 50, 183, 57);
-		scanPanel.add(label_1);
+		//errato
+		ImageIcon errato = new ImageIcon(Main.class.getResource("/Gui/images/greenbutton.png"));
+		JButton btnProdottoVisualizzatoErrato = new JButton("Prodotto corretto", errato);
+		btnProdottoVisualizzatoErrato.setVerticalTextPosition(JButton.CENTER);
+		btnProdottoVisualizzatoErrato.setHorizontalTextPosition(JButton.CENTER);		
+		btnProdottoVisualizzatoErrato.setBorderPainted(false);
+		btnProdottoVisualizzatoErrato.setMargin(new Insets(0, 0, 0, 0));
+		btnProdottoVisualizzatoErrato.setFont(new Font("Segoe UI Semibold", Font.BOLD, 20));
+		btnProdottoVisualizzatoErrato.setForeground(Color.BLACK);
+		btnProdottoVisualizzatoErrato.setContentAreaFilled(false);
+		btnProdottoVisualizzatoErrato.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				switchPanel(gttPanel);
+			}
+		});
+		btnProdottoVisualizzatoErrato.setOpaque(false);
+		btnProdottoVisualizzatoErrato.setForeground(Color.BLACK);
+		btnProdottoVisualizzatoErrato.setFont(new Font("Segoe UI Semibold", Font.BOLD, 20));
+		btnProdottoVisualizzatoErrato.setContentAreaFilled(false);
+		btnProdottoVisualizzatoErrato.setBorderPainted(false);
+		btnProdottoVisualizzatoErrato.setBounds(417, 466, 628, 57);
+		scanPanel.add(btnProdottoVisualizzatoErrato);
 		
 		JLabel label_2 = new JLabel("info");
-		label_2.setForeground(Color.WHITE);
+		label_2.setHorizontalAlignment(SwingConstants.CENTER);
+		label_2.setForeground(Color.BLACK);
 		label_2.setFont(new Font("Segoe UI Semibold", Font.BOLD, 30));
-		label_2.setBounds(926, 565, 73, 57);
+		label_2.setBounds(0, 571, 418, 57);
 		scanPanel.add(label_2);
 		
-		JButton btnNo = new JButton("NO");
-		btnNo.setOpaque(false);
-		btnNo.setForeground(Color.WHITE);
-		btnNo.setFont(new Font("Segoe UI Semibold", Font.BOLD, 20));
-		btnNo.setContentAreaFilled(false);
-		btnNo.setBorderPainted(false);
-		btnNo.setBounds(355, 543, 79, 57);
-		scanPanel.add(btnNo);
-		
-		JLabel lblProdottoScansionatoCorretto = new JLabel("Il prodotto scansionato \u00E8 corretto?");
-		lblProdottoScansionatoCorretto.setHorizontalAlignment(SwingConstants.CENTER);
-		lblProdottoScansionatoCorretto.setForeground(Color.WHITE);
-		lblProdottoScansionatoCorretto.setFont(new Font("Segoe UI Semibold", Font.BOLD, 25));
-		lblProdottoScansionatoCorretto.setBounds(0, 215, 711, 57);
-		scanPanel.add(lblProdottoScansionatoCorretto);
 		
 		JLabel lblImmagineProdotto = new JLabel("immagine prodotto");
-		lblImmagineProdotto.setBounds(220, 215, 269, 257);
+		lblImmagineProdotto.setHorizontalAlignment(SwingConstants.CENTER);
+		lblImmagineProdotto.setBounds(600, 128, 269, 257);
 		scanPanel.add(lblImmagineProdotto);
 		
 		JLabel label_3 = new JLabel("");
-		label_3.setIcon(new ImageIcon(GuiMain.class.getResource("/Gui/images/logo.png")));
-		label_3.setBounds(760, 117, 397, 414);
+		label_3.setHorizontalAlignment(SwingConstants.CENTER);
+		label_3.setIcon(new ImageIcon(Main.class.getResource("/Gui/images/logo.png")));
+		label_3.setBounds(0, 0, 418, 488);
 		scanPanel.add(label_3);
-		
-		JButton btnGetImg = new JButton("get IMG");
-		btnGetImg.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				
-			}
-		});
-		btnGetImg.setBounds(39, 312, 97, 25);
-		scanPanel.add(btnGetImg);
 		
 		
 		JButton btnPanel1 = new JButton("HOME");
+		btnPanel1.setBounds(60, 13, 97, 25);
 		btnPanel1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				switchPanel(homePanel);
 			}
 		});
-		btnPanel1.setBounds(60, 13, 97, 25);
 		contentPane.add(btnPanel1);
 		
 		JButton btnPanel2 = new JButton("ScProdotto");
+		btnPanel2.setBounds(189, 13, 97, 25);
 		btnPanel2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				switchPanel(scanPanel);
 			}
 		});
-		btnPanel2.setBounds(189, 13, 97, 25);
 		contentPane.add(btnPanel2);
 		
-		//
-		JButton btnScansionaProdotto = new JButton("");
-		btnScansionaProdotto.setIcon(new ImageIcon(GuiMain.class.getResource("/Gui/images/scanner.png")));
-		btnScansionaProdotto.setFont(new Font("Segoe UI Semibold", Font.BOLD, 20));
-		btnScansionaProdotto.setForeground(Color.WHITE);
-		//seguenti tre lineee per impostare JButton trasparente
-		btnScansionaProdotto.setOpaque(false);
-		btnScansionaProdotto.setContentAreaFilled(false);
+		//Avvia scansione
+		ImageIcon scan_icon = new ImageIcon(Main.class.getResource("/Gui/images/greenbutton.png"));
+		JButton btnScansionaProdotto = new JButton("Avvia scansione", scan_icon);
+		btnScansionaProdotto.setVerticalTextPosition(JButton.CENTER);
+		btnScansionaProdotto.setHorizontalTextPosition(JButton.CENTER);
+		
 		btnScansionaProdotto.setBorderPainted(false);
+		btnScansionaProdotto.setMargin(new Insets(0, 0, 0, 0));
+		//btnScansionaProdotto.setIcon(new ImageIcon(Main.class.getResource("/Gui/images/greenbutton.png")));
+		btnScansionaProdotto.setFont(new Font("Segoe UI Semibold", Font.BOLD, 20));
+		btnScansionaProdotto.setForeground(Color.BLACK);
+		btnScansionaProdotto.setContentAreaFilled(false);
 		btnScansionaProdotto.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				switchPanel(scanPanel);
@@ -268,7 +270,7 @@ public class GuiMain extends JFrame {
 					Connection con = DriverManager.getConnection(host, username, password);		//connessione
 
 					Statement stmtProdotto = con.createStatement();
-					String SQLProdotto = "SELECT * FROM prodotto WHERE barcode = '4006381115575'";
+					String SQLProdotto = "SELECT * FROM prodotto WHERE nome = 'levissima 50cl'";
 					ResultSet rsProdotto = stmtProdotto.executeQuery(SQLProdotto);
 					
 					//output
@@ -295,33 +297,34 @@ public class GuiMain extends JFrame {
 				}			
 			}
 		});
-		btnScansionaProdotto.setBounds(273, 350, 135, 96);
+		btnScansionaProdotto.setBounds(416, 212, 629, 96);
 		homePanel.add(btnScansionaProdotto);
 		
 		JLabel logo_label = new JLabel("");
-		logo_label.setIcon(new ImageIcon(GuiMain.class.getResource("/Gui/images/logo.png")));
-		logo_label.setBounds(760, 117, 397, 414);
+		logo_label.setHorizontalAlignment(SwingConstants.CENTER);
+		logo_label.setIcon(new ImageIcon(Main.class.getResource("/Gui/images/logo.png")));
+		logo_label.setBounds(0, 0, 418, 488);
 		homePanel.add(logo_label);
 		
-		JLabel lblScansionaProdotto = new JLabel("Scansiona Prodotto");
+		JLabel lblScansionaProdotto = new JLabel("Scansione Prodotto");
 		lblScansionaProdotto.setHorizontalAlignment(SwingConstants.CENTER);
-		lblScansionaProdotto.setForeground(Color.WHITE);
+		lblScansionaProdotto.setForeground(Color.BLACK);
 		lblScansionaProdotto.setFont(new Font("Segoe UI Semibold", Font.BOLD, 28));
-		lblScansionaProdotto.setBounds(0, 222, 710, 57);
+		lblScansionaProdotto.setBounds(416, 97, 629, 57);
 		homePanel.add(lblScansionaProdotto);
 		
 		JButton btnPanel3 = new JButton("GttProdotto");
+		btnPanel3.setBounds(309, 13, 97, 25);
 		btnPanel3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				switchPanel(gttPanel);
 			}
 		});
-		btnPanel3.setBounds(309, 13, 97, 25);
 		contentPane.add(btnPanel3);
 		
-		JLabel background = new JLabel("New label");
-		background.setIcon(new ImageIcon(GuiMain.class.getResource("/Gui/images/home.jpg")));
-		background.setBounds(0, -19, 1297, 766);
+		JLabel background = new JLabel("");
+		background.setBounds(0, -14, 1045, 761);
+		background.setIcon(new ImageIcon(Main.class.getResource("/Gui/images/home.jpg")));
 		contentPane.add(background);
 	}
 }
