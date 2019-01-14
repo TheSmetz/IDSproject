6a31e11b4328a23249af0f71a9da86002551f9c8
fala-flash <44972556+fala-flash@users.noreply.github.com>
2019-01-14 19:20:47 +0100

    Update dbtrash-it.sql

--- Eclipse/Database/dbtrash-it.sql
+++ Eclipse/Database/dbtrash-it.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Gen 14, 2019 alle 13:12
+-- Creato il: Gen 14, 2019 alle 19:18
 -- Versione del server: 10.1.37-MariaDB
 -- Versione PHP: 7.2.12
 
@@ -48,21 +48,19 @@ INSERT INTO `area` (`IDArea`, `Nome`) VALUES
 --
 
 CREATE TABLE `componente` (
-  `IDComponente` varchar(10) NOT NULL,
-  `Nome` varchar(20) NOT NULL
+  `IDcomponente` varchar(2) NOT NULL,
+  `prodottoID` varchar(13) NOT NULL,
+  `descrizione` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `componente`
 --
 
-INSERT INTO `componente` (`IDComponente`, `Nome`) VALUES
-('CA', 'Carta'),
-('IN', 'Indifferenziato'),
-('PL', 'Plastica'),
-('TB', 'Tappo Bottiglietta'),
-('TP', 'Tetrapack'),
-('VE', 'Vetro');
+INSERT INTO `componente` (`IDcomponente`, `prodottoID`, `descrizione`) VALUES
+('CA', '123456', 'corpo'),
+('PL', '123456', 'tappo'),
+('TP', '80007920', 'corpo');
 
 -- --------------------------------------------------------
 
@@ -71,20 +69,22 @@ INSERT INTO `componente` (`IDComponente`, `Nome`) VALUES
 --
 
 CREATE TABLE `policy` (
-  `IDArea` varchar(2) NOT NULL,
-  `IDComponente` varchar(10) NOT NULL,
-  `Descrizione` varchar(20) NOT NULL
+  `componenteID` varchar(2) NOT NULL,
+  `areaID` varchar(2) NOT NULL,
+  `descrizione` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `policy`
 --
 
-INSERT INTO `policy` (`IDArea`, `IDComponente`, `Descrizione`) VALUES
-('MC', 'TP', 'Plastica'),
-('MC', 'TP', 'Plastica'),
-('AP', 'TP', 'Carta'),
-('AP', 'TP', 'Carta');
+INSERT INTO `policy` (`componenteID`, `areaID`, `descrizione`) VALUES
+('CA', 'AP', 'carta'),
+('CA', 'MC', 'carta'),
+('PL', 'AP', 'plastica'),
+('PL', 'MC', 'plastica'),
+('TP', 'AP', 'carta'),
+('TP', 'MC', 'indifferenziato');
 
 -- --------------------------------------------------------
 
@@ -94,20 +94,16 @@ INSERT INTO `policy` (`IDArea`, `IDComponente`, `Descrizione`) VALUES
 
 CREATE TABLE `prodotto` (
   `IDProdotto` varchar(13) NOT NULL,
-  `Nome` varchar(20) NOT NULL,
-  `PrimoComponente` varchar(20) NOT NULL,
-  `SecondoComponente` varchar(20) DEFAULT NULL,
-  `TerzoComponente` varchar(20) DEFAULT NULL,
-  `QuartoComponente` varchar(20) DEFAULT NULL
+  `Nome` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `prodotto`
 --
 
-INSERT INTO `prodotto` (`IDProdotto`, `Nome`, `PrimoComponente`, `SecondoComponente`, `TerzoComponente`, `QuartoComponente`) VALUES
-('123456', 'Bottiglia Latte', 'TP', NULL, NULL, NULL),
-('80007920', 'San Benedetto 2L', 'PL', 'TB', NULL, NULL);
+INSERT INTO `prodotto` (`IDProdotto`, `Nome`) VALUES
+('123456', 'Bottiglia Latte'),
+('80007920', 'San Benedetto 2L');
 
 --
 -- Indici per le tabelle scaricate
@@ -123,44 +119,38 @@ ALTER TABLE `area`
 -- Indici per le tabelle `componente`
 --
 ALTER TABLE `componente`
-  ADD PRIMARY KEY (`IDComponente`);
+  ADD PRIMARY KEY (`IDcomponente`,`prodottoID`),
+  ADD KEY `prodottoID` (`prodottoID`);
 
 --
 -- Indici per le tabelle `policy`
 --
 ALTER TABLE `policy`
-  ADD KEY `AreaID` (`IDArea`),
-  ADD KEY `ComponenteID` (`IDComponente`);
+  ADD PRIMARY KEY (`componenteID`,`areaID`),
+  ADD KEY `areaID` (`areaID`);
 
 --
 -- Indici per le tabelle `prodotto`
 --
 ALTER TABLE `prodotto`
-  ADD PRIMARY KEY (`IDProdotto`),
-  ADD KEY `PComponente` (`PrimoComponente`),
-  ADD KEY `SComponente` (`SecondoComponente`),
-  ADD KEY `TComponente` (`TerzoComponente`),
-  ADD KEY `QComponente` (`QuartoComponente`);
+  ADD PRIMARY KEY (`IDProdotto`);
 
 --
 -- Limiti per le tabelle scaricate
 --
 
 --
+-- Limiti per la tabella `componente`
+--
+ALTER TABLE `componente`
+  ADD CONSTRAINT `componente_ibfk_1` FOREIGN KEY (`prodottoID`) REFERENCES `prodotto` (`IDProdotto`);
+
+--
 -- Limiti per la tabella `policy`
 --
 ALTER TABLE `policy`
-  ADD CONSTRAINT `AreaID` FOREIGN KEY (`IDArea`) REFERENCES `area` (`IDArea`),
-  ADD CONSTRAINT `ComponenteID` FOREIGN KEY (`IDComponente`) REFERENCES `componente` (`IDComponente`);
-
---
--- Limiti per la tabella `prodotto`
---
-ALTER TABLE `prodotto`
-  ADD CONSTRAINT `PComponente` FOREIGN KEY (`PrimoComponente`) REFERENCES `componente` (`IDComponente`),
-  ADD CONSTRAINT `QComponente` FOREIGN KEY (`QuartoComponente`) REFERENCES `componente` (`IDComponente`),
-  ADD CONSTRAINT `SComponente` FOREIGN KEY (`SecondoComponente`) REFERENCES `componente` (`IDComponente`),
-  ADD CONSTRAINT `TComponente` FOREIGN KEY (`TerzoComponente`) REFERENCES `componente` (`IDComponente`);
+  ADD CONSTRAINT `policy_ibfk_1` FOREIGN KEY (`componenteID`) REFERENCES `componente` (`IDcomponente`),
+  ADD CONSTRAINT `policy_ibfk_2` FOREIGN KEY (`areaID`) REFERENCES `area` (`IDArea`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
