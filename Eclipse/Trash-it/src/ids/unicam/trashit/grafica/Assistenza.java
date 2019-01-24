c269ac2f8f9c0c970e3507240f8881980d9580ec
TheSmetz <44972483+TheSmetz@users.noreply.github.com>
2019-01-24 12:11:16 +0100

    Update Assistenza.java

--- Eclipse/Trash-it/src/ids/unicam/trashit/grafica/Assistenza.java
+++ Eclipse/Trash-it/src/ids/unicam/trashit/grafica/Assistenza.java
@@ -2,28 +2,22 @@ package ids.unicam.trashit.grafica;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
-import javax.swing.text.StyledDocument;
 
-public class Assistenza extends JFrame {
 
-	JPanel assistenza;
-	JLabel asslblAssistenza;
-	JTextPane asstxtpnTelefonoEmail;
+public class Assistenza {
+
+	private JPanel assistenza;
+	private JLabel asslblAssistenza;
+	private JTextPane asstxtpnTelefonoEmail;
+	private Home h;
+	private About a;
 
 	private void lblAssistenza() {
-		JLabel asslblAssistenza = new JLabel("ASSISTENZA");
+		asslblAssistenza = new JLabel("ASSISTENZA");
 		asslblAssistenza.setHorizontalAlignment(SwingConstants.CENTER);
 		asslblAssistenza.setForeground(Color.BLACK);
 		asslblAssistenza.setFont(new Font("Segoe UI Semibold", Font.BOLD, 30));
@@ -42,20 +36,22 @@ public class Assistenza extends JFrame {
 		assistenza.add(asstxtpnTelefonoEmail);
 	}
 	
-	private void setJPanelAssistenza() {
+	@SuppressWarnings("unused")
+	public void setJPanelAssistenza() {
 		assistenza = new JPanel();
 		assistenza.setLayout(null);
 		assistenza.setOpaque(false);
 		lblAssistenza();
-		txtpnTelefonoEmail();		
-		Home.btnInfo(assistenza);		
-		Home.lblLogo(assistenza);
-		Home.btnChiudiSessione(assistenza);
-		About.btnHomePage(assistenza);
+		txtpnTelefonoEmail();	
+		h=new Home();
+		h.btnInfo(assistenza);		
+		h.lblLogo(assistenza);
+		h.btnChiudiSessione(assistenza);
+		a=new About();
+		a.btnHomePage(assistenza);
 	}
 	
 	public JPanel getJPanelAssistenza() {
-		setJPanelAssistenza();
-		return assistenza;
+		return this.assistenza;
 	}
 }
